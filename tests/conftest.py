674997bcc3002875e9ae237e84f1eb3a3c6e3462
drc38 <derek_Caudwell@hotmail.com>
2021-07-11 23:04:16 +1200

    extend charge point tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,8 +29,8 @@ def skip_notifications_fixture():
 @pytest.fixture(name="bypass_get_data")
 def bypass_get_data_fixture():
     """Skip calls to get data from API."""
-    # with patch("custom_components.ocpp.ocppApiClient.async_get_data"):
-    #    yield
+    with patch("custom_components.ocpp.api.async_get_data"):
+        yield
 
 
 # In this fixture, we are forcing calls to async_get_data to raise an Exception. This is useful
@@ -38,8 +38,8 @@ def bypass_get_data_fixture():
 @pytest.fixture(name="error_on_get_data")
 def error_get_data_fixture():
     """Simulate error when retrieving data from API."""
-    # with patch(
-    #    "custom_components.ocpp.ocppApiClient.async_get_data",
-    #    side_effect=Exception,
-    # ):
-    #    yield
+    with patch(
+        "custom_components.ocpp.api.async_get_data",
+        side_effect=Exception,
+    ):
+        yield
--- tests/test_charge_point.py
+++ tests/test_charge_point.py
@@ -1,26 +1,52 @@
 """Implement a test by a simulating a chargepoint."""
 import asyncio
 from datetime import datetime, timezone
-import logging
 
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 import websockets
 
+from custom_components.ocpp import async_setup_entry
+from custom_components.ocpp.const import DOMAIN
 from ocpp.v16 import ChargePoint as cp, call
 from ocpp.v16.enums import AuthorizationStatus, RegistrationStatus
 
-logging.basicConfig(level=logging.INFO)
+from .const import MOCK_CONFIG_DATA
+
+
+async def test_cms_responses(hass):
+    """Test central system responses to a charger."""
+    # Create a mock entry so we don't have to go through config flow
+    config_entry = MockConfigEntry(
+        domain=DOMAIN, data=MOCK_CONFIG_DATA, entry_id="test"
+    )
+    assert await async_setup_entry(hass, config_entry)
+    await hass.async_block_till_done()
+
+    async with websockets.connect(
+        "ws://localhost:9000/CP_1", subprotocols=["ocpp1.6"]
+    ) as ws:
+
+        cp = ChargePoint("CP_1", ws)
+
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
+        await cp.send_start_transaction()
+        await cp.send_stop_transaction()
 
 
 class ChargePoint(cp):
     """Representation of Charge Point."""
+
+    def __init__(self, id, connection, response_timeout=30):
+        """Init extra variables for testing."""
+        self._transactionId = 0
 
     async def send_boot_notification(self):
         """Send a boot notification."""
         request = call.BootNotificationPayload(
             charge_point_model="Optimus", charge_point_vendor="The Mobility House"
         )
-        response = await self.call(request)
-        assert response.status == RegistrationStatus.accepted
+        resp = await self.call(request)
+        assert resp.status == RegistrationStatus.accepted
 
     async def send_start_transaction(self):
         """Send a start transaction notification."""
@@ -30,20 +56,21 @@ class ChargePoint(cp):
             meter_start=12345,
             timestamp=datetime.now(tz=timezone.utc).isoformat,
         )
-        response = await self.call(request)
-        assert response.status == AuthorizationStatus.accepted
+        resp = await self.call(request)
+        self._transactionId = resp.transaction_id
+        assert resp.id_tag_info["status"] == AuthorizationStatus.accepted.value
 
     async def send_stop_transaction(self):
         """Send a stop transaction notification."""
         request = call.StopTransactionPayload(
             meter_stop=54321,
             timestamp=datetime.now(tz=timezone.utc).isoformat,
-            transaction_id=123,
+            transaction_id=self._transactionId,
             reason="EVDisconnected",
             id_tag="test_cp",
         )
-        response = await self.call(request)
-        assert response.status == AuthorizationStatus.accepted
+        resp = await self.call(request)
+        assert resp.id_tag_info["status"] == AuthorizationStatus.accepted.value
 
 
 async def main():
