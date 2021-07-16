5399b58d898d2bccd6c4ce0deb1fb0d43f5d9d79
drc38 <20024196+drc38@users.noreply.github.com>
2021-07-16 15:10:12 +1200

    Update test_charge_point.py

--- tests/test_charge_point.py
+++ tests/test_charge_point.py
@@ -58,8 +58,8 @@ async def test_cms_responses(hass):
                 cp.send_meter_data(),
                 cp.send_stop_transaction(),
                 cs.charge_points["test_cpid"].unlock(),
-                cs.charge_points["test_cpid"].reset(),
-                cs.charge_points["test_cpid"].set_charge_rate(),
+                #cs.charge_points["test_cpid"].reset(),
+                #cs.charge_points["test_cpid"].set_charge_rate(),
                 cs.charge_points["test_cpid"].start_transaction()), timeout = 7,
                                   )
         except asyncio.TimeoutError:
