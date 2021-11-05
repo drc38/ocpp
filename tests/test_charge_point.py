dd9822ebf0a714a16f29c35794e83793c73427c3
drc38 <derek_Caudwell@hotmail.com>
2021-11-06 12:36:26 +1300

    enable sockets in testing

--- tests/test_charge_point.py
+++ tests/test_charge_point.py
@@ -34,7 +34,7 @@ from ocpp.v16.enums import (
 from .const import MOCK_CONFIG_DATA
 
 
-async def test_cms_responses(hass):
+async def test_cms_responses(hass, socket_enabled):
     """Test central system responses to a charger."""
 
     async def test_switches(hass):
